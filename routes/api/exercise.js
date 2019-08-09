8c965875b9667c49a6156f9e4aa832e9ee5a9415
jefferson <cardbeatt@gmail.com>
2019-08-09 12:20:34 -0300

    added routes

---
+++ routes/api/exercise.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/exercise", (req, res) => {
+  console.log("soulzilene is the true villain");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express"),
   bodyParser = require("body-parser"),
-  cors = require("cors");
+  cors = require("cors"),
+  api = require("./routes/api/exercise");
 
 const app = express();
 var port = process.env.PORT || 3000;
@@ -15,4 +16,23 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("index");
+});
+
+app.use("/api", api);
+
+app.use((err, req, res, next) => {
+  let errCode, errMessage;
+
+  if (err.errors) {
+    errCode = 400;
+    const keys = Object.keys(err.errors);
+    errMessage = err.errors[key[0]].message;
+  } else {
+    errcode = err.status || 500;
+    errMessage = err.message || "Internal Server Error";
+  }
+  res
+    .status(errCode)
+    .type("txt")
+    .send(errMessage);
 });
